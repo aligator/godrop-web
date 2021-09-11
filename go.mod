88abc39e886d8631590dbc53691340971840cb67
aligator <aligator@suncraft-server.de>
2021-09-11 15:12:24 +0200

    Use GoDrop v0.1.3

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/aligator/godrop-web
 
 go 1.16
 
-require github.com/aligator/godrop v0.1.0
+require (
+	github.com/aligator/godrop v0.1.3
+	github.com/go-chi/chi/v5 v5.0.3
+)
--- godrop.go
+++ godrop.go
@@ -2,17 +2,76 @@ package main
 
 import (
 	"embed"
+	"errors"
 	"github.com/aligator/godrop/server"
+	"github.com/aligator/godrop/server/log"
+	"github.com/aligator/godrop/server/provider"
+	"github.com/go-chi/chi/v5"
 	"io/fs"
+	"net/http"
+	"os"
+	"strings"
 )
 
 //go:embed build
 var frontend embed.FS
 
-func main() {
+func serveFrontend(router *chi.Mux) {
 	subFS, err := fs.Sub(frontend, "build")
 	if err != nil {
 		panic(err)
 	}
-	server.Run(subFS)
+	httpFS := http.FileServer(http.FS(subFS))
+	router.Handle("/*", http.StripPrefix("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer httpFS.ServeHTTP(w, r)
+
+		if r.URL.Path == "" {
+			r.URL.Path = "/"
+			return
+		}
+
+		f, err := subFS.Open(strings.TrimSuffix(r.URL.Path, "/"))
+		if errors.Is(err, fs.ErrNotExist) {
+			r.URL.Path = "/"
+			return
+		} else if err != nil {
+			panic(err)
+		} else {
+			err := f.Close()
+			if err != nil {
+				panic(err)
+			}
+		}
+	})))
+}
+
+func main() {
+	logger := log.DefaultLogger()
+
+	filesLocation := os.Getenv("GODROP_FILES")
+	if filesLocation == "" {
+		filesLocation = "./files"
+	}
+
+	repos, err := provider.NewDefaultRepos(filesLocation)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	s := server.Server{
+		Host:           os.Getenv("GODROP_HOST"),
+		AllowedOrigins: []string{"*"},
+		Repos:          repos,
+		Logger:         logger,
+	}
+
+	if s.Host == "" {
+		// Set localhost as default else windows needs to open the firewall
+		// for each new compiled binary, which is very annoying in development.
+		s.Host = "localhost:8080"
+	}
+
+	router := s.Init()
+	serveFrontend(router)
+	s.Run()
 }
